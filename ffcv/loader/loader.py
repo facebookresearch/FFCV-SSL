dc91723783ae223ced85a3397cf8ef769201a436
Florian Bordes <fbordes@fb.com>
2022-10-28 09:27:38 -0700

    Add new reader and loader

--- ffcv/loader/loader.py
+++ ffcv/loader/loader.py
@@ -55,7 +55,7 @@ class Loader:
 
     Parameters
     ----------
-    fname: str
+    path: str
         Full path to the location of the dataset (.beton file format).
     batch_size : int
         Batch size.
@@ -86,52 +86,53 @@ class Loader:
     recompile : bool
         Recompile every iteration. This is necessary if the implementation of some augmentations are expected to change during training.
     """
-    def __init__(self,
-                 fname: str,
-                 batch_size: int,
-                 num_workers: int = -1,
-                 os_cache: bool = DEFAULT_OS_CACHE,
-                 order: ORDER_TYPE = OrderOption.SEQUENTIAL,
-                 distributed: bool = False,
-                 seed: int = None,  # For ordering of samples
-                 indices: Sequence[int] = None,  # For subset selection
-                 pipelines: Mapping[str,
-                                    Sequence[Union[Operation, ch.nn.Module]]] = {},
-                 custom_fields: Mapping[str, Type[Field]] = {},
-                 drop_last: bool = True,
-                 batches_ahead: int = 3,
-                 recompile: bool = False,  # Recompile at every epoch
-                 ):
 
-        if distributed and order == OrderOption.RANDOM and (seed is None):
-            print('Warning: no ordering seed was specified with distributed=True. '
-                  'Setting seed to 0 to match PyTorch distributed sampler.')
-            seed = 0
-        elif seed is None:
-            tinfo = np.iinfo('int32')
-            seed = np.random.randint(0, tinfo.max)
+    def __init__(
+        self,
+        path: str,
+        batch_size: int,
+        order: ORDER_TYPE,
+        num_workers: int = -1,
+        os_cache: bool = DEFAULT_OS_CACHE,
+        distributed: bool = False,
+        seed: int = None,  # For ordering of samples
+        indices: Sequence[int] = None,  # For subset selection
+        pipelines: Mapping[str, Sequence[Union[Operation, ch.nn.Module]]] = {},
+        custom_fields: Mapping[str, Type[Field]] = {},
+        drop_last: bool = True,
+        batches_ahead: int = 3,
+        recompile: bool = False,  # Recompile at every epoch
+        custom_field_mapper: int = None,
+    ):
 
         # We store the original user arguments to be able to pass it to the
         # filtered version of the datasets
+        if seed is None:
+            import os
+
+            random_data = os.urandom(4)
+            seed = int.from_bytes(random_data, byteorder="big")
+
         self._args = {
-            'fname': fname,
-            'batch_size': batch_size,
-            'num_workers': num_workers,
-            'os_cache': os_cache,
-            'order': order,
-            'distributed': distributed,
-            'seed': seed,
-            'indices': indices,
-            'pipelines': pipelines,
-            'drop_last': drop_last,
-            'batches_ahead': batches_ahead,
-            'recompile': recompile
+            "path": path,
+            "batch_size": batch_size,
+            "num_workers": num_workers,
+            "os_cache": os_cache,
+            "order": order,
+            "distributed": distributed,
+            "seed": seed,
+            "indices": indices,
+            "pipelines": pipelines,
+            "drop_last": drop_last,
+            "batches_ahead": batches_ahead,
+            "recompile": recompile,
+            "custom_field_mapper": custom_field_mapper,
         }
-        self.fname: str = fname
+        self.path: str = path
         self.batch_size: int = batch_size
         self.batches_ahead = batches_ahead
         self.seed: int = seed
-        self.reader: Reader = Reader(self.fname, custom_fields)
+        self.reader: Reader = Reader(self.path, custom_fields, custom_field_mapper)
         self.num_workers: int = num_workers
         self.drop_last: bool = drop_last
         self.distributed: bool = distributed
@@ -144,15 +145,14 @@ class Loader:
         Compiler.set_num_threads(self.num_workers)
 
         if indices is None:
-            self.indices = np.arange(self.reader.num_samples, dtype='uint64')
+            self.indices = np.arange(self.reader.num_samples, dtype="uint64")
         else:
             self.indices = np.array(indices)
 
         if os_cache:
             self.memory_manager: MemoryManager = OSCacheManager(self.reader)
         else:
-            self.memory_manager: MemoryManager = ProcessCacheManager(
-                self.reader)
+            self.memory_manager: MemoryManager = ProcessCacheManager(self.reader)
 
         self.traversal_order: TraversalOrder = ORDER_MAP[order](self)
 
@@ -163,6 +163,8 @@ class Loader:
         self.field_name_to_f_ix = {}
 
         for f_ix, (field_name, field) in enumerate(self.reader.handlers.items()):
+            if f_ix > 1:
+                f_ix = 0
             self.field_name_to_f_ix[field_name] = f_ix
             DecoderClass = field.get_decoder_class()
             try:
@@ -176,25 +178,18 @@ class Loader:
                     msg += f"{DecoderClass}"
                     raise ValueError(msg)
             except KeyError:
-                try:
-                    operations = [
-                        DecoderClass(),
-                        ToTensor()
-                    ]
-                except Exception:
-                    msg = f"Impossible to create a default pipeline"
-                    msg += f"{field_name}, please define one manually"
-                    raise ValueError(msg)
-
+                msg = f"{field_name}, please define a pipeline"
+                raise ValueError(msg)
+            # we loop over the operations of this field
+            # and wrap if the op is a torch module
             for i, op in enumerate(operations):
                 assert isinstance(op, (ch.nn.Module, Operation)), op
                 if isinstance(op, ch.nn.Module):
                     operations[i] = ModuleWrapper(op)
-
-            for op in operations:
-                op.accept_field(field)
-                op.accept_globals(self.reader.metadata[f'f{f_ix}'],
-                                  memory_read)
+                operations[i].accept_field(field)
+                operations[i].accept_globals(
+                    self.reader.metadata[f"f{f_ix}"], memory_read
+                )
 
             self.pipelines[field_name] = Pipeline(operations)
 
@@ -204,7 +199,7 @@ class Loader:
     def __iter__(self):
         Compiler.set_num_threads(self.num_workers)
         order = self.next_traversal_order()
-        selected_order = order[:len(self) * self.batch_size]
+        selected_order = order[: len(self) * self.batch_size]
         self.next_epoch += 1
 
         # Compile at the first epoch
@@ -213,7 +208,7 @@ class Loader:
 
         return EpochIterator(self, selected_order)
 
-    def filter(self, field_name:str, condition: Callable[[Any], bool]) -> 'Loader':
+    def filter(self, field_name: str, condition: Callable[[Any], bool]) -> "Loader":
         new_args = {**self._args}
         pipelines = {}
 
@@ -223,18 +218,18 @@ class Loader:
 
         # We reuse the original pipeline for the field we care about
         try:
-            pipelines[field_name] = new_args['pipelines'][field_name]
+            pipelines[field_name] = new_args["pipelines"][field_name]
         except KeyError:
             # We keep the default one if the user didn't setup a custom one
             del pipelines[field_name]
             pass
 
-        new_args['pipelines'] = pipelines
+        new_args["pipelines"] = pipelines
 
         # We use sequential order for speed and to know which index we are
         # filtering
-        new_args['order'] = OrderOption.SEQUENTIAL
-        new_args['drop_last'] = False
+        new_args["order"] = OrderOption.SEQUENTIAL
+        new_args["drop_last"] = False
         sub_loader = Loader(**new_args)
         selected_indices = []
 
@@ -246,9 +241,8 @@ class Loader:
                     selected_indices.append(sample_id)
 
         final_args = {**self._args}
-        final_args['indices'] = np.array(selected_indices)
+        final_args["indices"] = np.array(selected_indices)
         return Loader(**final_args)
-
 
     def __len__(self):
         next_order = self.next_traversal_order()
@@ -257,50 +251,62 @@ class Loader:
         else:
             return int(np.ceil(len(next_order) / self.batch_size))
 
-    def generate_function_call(self, pipeline_name, op_id, needs_indices):
+    def generate_function_call(
+        self, pipeline_name, op_id, needs_indices, needs_counter
+    ):
         p_ix = self.field_name_to_f_ix[pipeline_name]
-        pipeline_identifier = f'code_{pipeline_name}_{op_id}'
-        memory_identifier = f'memory_{pipeline_name}_{op_id}'
-        result_identifier = f'result_{pipeline_name}'
+        pipeline_identifier = f"code_{pipeline_name}_{op_id}"
+        memory_identifier = f"memory_{pipeline_name}_{op_id}"
+        result_identifier = f"result_{pipeline_name}"
 
         arg_id = result_identifier
         # This is the decoder so we pass the indices instead of the previous
         # result
         if op_id == 0:
-            arg_id = 'batch_indices'
+            arg_id = "batch_indices"
 
-        tree = ast.parse(f"""
+        tree = ast.parse(
+            f"""
 {result_identifier} = {pipeline_identifier}({arg_id}, {memory_identifier})
-        """).body[0]
+        """
+        ).body[0]
 
         # This is the first call of the pipeline, we pass the metadata and
         # storage state
         if op_id == 0:
-            tree.value.args.extend([
-                ast.Subscript(value=ast.Name(id='metadata', ctx=ast.Load()),
-                              slice=ast.Index(value=ast.Constant(value=f'f{p_ix}', kind=None)), ctx=ast.Load()),
-                ast.Name(id='storage_state', ctx=ast.Load()),
-            ])
+            tree.value.args.append(
+                ast.Subscript(
+                    value=ast.Name(id="metadata", ctx=ast.Load()),
+                    slice=ast.Index(value=ast.Constant(value=f"f{p_ix}", kind=None)),
+                    ctx=ast.Load(),
+                )
+            )
+            tree.value.args.append(ast.Name(id="storage_state", ctx=ast.Load()))
         if needs_indices:
-            tree.value.args.extend([
-                ast.Name(id='batch_indices', ctx=ast.Load()),
-            ])
+            tree.value.args.append(ast.Name(id="batch_indices", ctx=ast.Load()))
+        if needs_counter:
+            tree.value.args.append(ast.Name(id="counter", ctx=ast.Load()))
         return tree
 
     def generate_stage_code(self, stage, stage_ix, functions):
-        fun_name = f'stage_{stage_ix}'
-        base_code = ast.parse(f"""
+        fun_name = f"stage_{stage_ix}"
+        base_code = ast.parse(
+            f"""
 def {fun_name}():
     pass
-        """).body[0]
+        """
+        ).body[0]
 
         function_calls = []
         memory_banks = []
         memory_banks_id = []
-        for p_ix, pipeline_name, op_id, needs_indices in stage:
-            function_calls.append(self.generate_function_call(pipeline_name,
-                                                              op_id, needs_indices))
-            arg = ast.arg(arg=f'memory_{pipeline_name}_{op_id}')
+        for pipeline_name, op_id, needs_indices, needs_counter in stage:
+            function_calls.append(
+                self.generate_function_call(
+                    pipeline_name, op_id, needs_indices, needs_counter
+                )
+            )
+            arg = ast.arg(arg=f"memory_{pipeline_name}_{op_id}")
             memory_banks.append(arg)
             memory_banks_id.append((pipeline_name, op_id))
 
@@ -309,65 +315,63 @@ def {fun_name}():
 
         return_tuple = ast.Return(value=ast.Tuple(elts=[], ctx=ast.Load()))
 
-        base_code.args.args.append(ast.arg(arg='batch_indices'))
+        base_code.args.args.append(ast.arg(arg="batch_indices"))
+        base_code.args.args.append(ast.arg(arg="counter"))
 
         for p_id in self.pipelines.keys():
-            r = f'result_{p_id}'
+            r = f"result_{p_id}"
             if stage_ix != 0:
                 base_code.args.args.append(ast.arg(arg=r))
             return_tuple.value.elts.append(ast.Name(id=r, ctx=ast.Load()))
 
-
         base_code.body.append(return_tuple)
         base_code.args.args.extend(memory_banks)
-        base_code.args.args.append(ast.arg(arg='metadata'))
-        base_code.args.args.append(ast.arg(arg='storage_state'))
+        base_code.args.args.append(ast.arg(arg="metadata"))
+        base_code.args.args.append(ast.arg(arg="storage_state"))
 
         module = ast.fix_missing_locations(
-            ast.Module(body=[base_code],
-                       type_ignores=[])
+            ast.Module(body=[base_code], type_ignores=[])
         )
         namespace = {
             **functions,
         }
 
-        exec(compile(module, '', 'exec'), namespace)
+        exec(compile(module, "", "exec"), namespace)
         final_code = namespace[fun_name]
-        if stage_ix % 2 == 0:
+        if stage_ix % 2 == 0:  # is in jit-mode
             final_code = Compiler.compile(final_code)
 
         return final_code, memory_banks_id
 
     def generate_code(self):
-        schedule = defaultdict(lambda: [])
+
+        self.memory_bank_keys_per_stage = {}
+        self.code_per_stage = {}
         compiled_functions = {}
-        for p_ix, (p_id, p) in enumerate(self.pipelines.items()):
-            stage = 0
-            for jitted_block, block_content in p.operation_blocks:
-                # Even stages are jitted Odds are not
-                # If this doesn't match for this pipeline we
-                # shift the operations
-                if 1 - jitted_block % 2 != stage % 2:
-                    stage += 1
+        schedules = defaultdict(lambda: [])
+        # the pipelines are grouped per block of what can be compiled or not
+        for p_id, p in self.pipelines.items():
+            # ensure the first is jittable
+            assert p.operation_blocks[0][0] == True
+            for block_i, (_, block_content) in enumerate(p.operation_blocks):
                 for op in block_content:
                     ops_code = p.compiled_ops[op]
 
                     needs_indices = False
-                    if hasattr(ops_code, 'with_indices'):
+                    needs_counter = False
+                    if hasattr(ops_code, "with_indices"):
                         needs_indices = ops_code.with_indices
+                    if hasattr(ops_code, "with_counter"):
+                        needs_counter = ops_code.with_counter
 
-                    if stage % 2 == 0:
+                    if block_i % 2 == 0:
                         ops_code = Compiler.compile(ops_code)
-                    compiled_functions[f'code_{p_id}_{op}'] = ops_code
-                    schedule[stage].append((p_ix, p_id, op, needs_indices))
-                stage += 1
+                    compiled_functions[f"code_{p_id}_{op}"] = ops_code
+                    schedules[block_i].append((p_id, op, needs_indices, needs_counter))
 
-        memory_bank_keys_per_stage = {}
-        self.code_per_stage = {}
-        for stage_ix, stage in schedule.items():
-            code_for_stage, mem_banks_ids = self.generate_stage_code(stage, stage_ix,
-                                                                     compiled_functions)
-            self.code_per_stage[stage_ix] = code_for_stage
-            memory_bank_keys_per_stage[stage_ix] = mem_banks_ids
-
-        self.memory_bank_keys_per_stage = memory_bank_keys_per_stage
+        for i, stage in schedules.items():
+            code_for_stage, mem_banks_ids = self.generate_stage_code(
+                stage, i, compiled_functions
+            )
+            self.code_per_stage[i] = code_for_stage
+            self.memory_bank_keys_per_stage[i] = mem_banks_ids
